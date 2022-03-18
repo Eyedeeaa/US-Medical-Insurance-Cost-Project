ca78a57763069483fa112188d0a8d16346f6d599
Danny Rodriguez <danny4386@gmail.com>
2022-03-17 18:23:22 -0700

    added some more functions for analysis and sorting patients by categories

--- us_medical_insurance_cost.py
+++ us_medical_insurance_cost.py
@@ -6,6 +6,7 @@ ages = []
 sex = []
 bmis = []
 num_children = []
+smoker_status = []
 region = []
 insurance_cost = []
 patient_data = {}
@@ -14,17 +15,17 @@ patient_data = {}
 #Parse insurance CSV
 with open("insurance.csv") as ic_csv:
     patient_data_dict = csv.DictReader(ic_csv)
-    for row in patient_data_dict:
+    for row_num, row in enumerate(patient_data_dict):
         #Fill individual lists
         ages.append(int(row['age']))
         sex.append(row['sex'])
         bmis.append(float(row['bmi']))
         num_children.append(int(row['children']))
+        smoker_status.append(row['smoker'])
         region.append(row['region'])
         insurance_cost.append(float(row['charges']))
-
-    # Create patient dictionary
-    for row_num, row in enumerate(patient_data_dict):
+        
+        # Create patient dictionary
         patient_num = row_num
         patient_data.update({
             patient_num: {
@@ -33,13 +34,11 @@ with open("insurance.csv") as ic_csv:
                 'Sex': row['sex'],
                 'BMI': float(row['bmi']),
                 'Children': int(row['children']),
+                'Smoker Status': row['smoker'],
                 'Region': row['region'],
                 'Insurance Cost': float(row['charges'])
             }
         })
-
-
-
 
 
 #Function to find averages
@@ -60,7 +59,6 @@ southwest = []
 southeast = []
 
 def sort_region(patients):
-
     for patient in patients:
         if patients[patient]['Region'] == 'northwest':
             northwest.append(patients[patient])
@@ -72,4 +70,64 @@ def sort_region(patients):
             southeast.append(patients[patient])
 
 sort_region(patient_data)
+
+num_northwest = len(northwest)
+num_northeast = len(northeast)
+num_southwest = len(southwest)
+num_southeast = len(southeast)
+
+
+# Find male to female ratio
+def m_to_f_ratio(lst):
+    male = 0
+    female = 0
+    for sex in lst:
+        if sex == 'male':
+            male += 1
+        else:
+            female += 1
+    return male, female
+
+male_count, female_count = m_to_f_ratio(sex)
+
+#Classify BMI rating
+
+under_weight_range = []
+health_weight_range = []
+over_weight_range = []
+obese_range = {'Class-1': [], 'Class-2': [], 'Class-3': []}
+
+def sort_bmi(dict):
+    for patient in dict.values():
+        patient_bmi = patient['BMI']
+        if patient_bmi < 18.5:
+            under_weight_range.append(patient)
+        elif patient_bmi >= 18.5 and patient_bmi < 25:
+            health_weight_range.append(patient)
+        elif patient_bmi >= 25 and patient_bmi < 30:
+            over_weight_range.append(patient)
+        else:
+            if patient_bmi > 30 and patient_bmi < 35:
+                obese_range['Class-1'].append(patient)
+            elif patient_bmi >= 35 and patient_bmi < 40:
+                obese_range['Class-2'].append(patient)
+            else:
+                obese_range['Class-3'].append(patient)
+
+
+sort_bmi(patient_data)
+
+#Smoker VS Non-Smoker Ratio
+smokers = []
+non_smokers = []
+def sort_by_smoker_status(dict):
+    for patient in dict.values():
+        if patient['Smoker Status'] == "yes":
+            smokers.append(patient)
+        else:
+            non_smokers.append(patient)
+
+sort_by_smoker_status(patient_data)
+print(len(non_smokers))
+
 
